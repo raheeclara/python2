8fc05648a23104381f5203a721afa6adfa0861ad
raheeclara <138274269+raheeclara@users.noreply.github.com>
2023-07-15 14:43:04 +0900

    Add files via upload

--- python.test.py
+++ python.test.py
@@ -1,13 +1,15 @@
-from PIL import Image
-from PIL import ImageDraw
-canvas_size = (200,200)
-rect_area = [(0,0), (100,100)]
-line_cor = [(0,200), (200,0)]
-circle_area = [(100,100),(200,200)]
-new_img = Image.new("RGB",canvas_size,"orange")
-draw_imgs = ImageDraw.Draw(new_img)
-draw_imgs.rectangle(rect_area,fill = "red",outline = "yellow")
-draw_imgs.line(line_cor,fill = "blue",width = 4)
-draw_imgs.ellipse(circle_area,fill = "green",outline = "red")
-new_img.show()
-new_img.save("imagedraw.jpg")
+class Car:
+    def __init__(self,model,color):
+        self.model = model
+        self.color = color
+    def info(self):
+        print("Model :",self.model,",Color:",self.color)
+class CarDrive(Car):
+    def info(self):
+        print("The model of this car is %s."% self.model)
+        print("The color is %s."% self.color)
+    def speedChange(self,v):
+        self.speed = v
+        print("speedChange :",self.speed)    
+bmw = CarDrive("BMW","white")
+bmw.info()
