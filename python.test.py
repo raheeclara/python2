b1cf2ded50dccb055809658ae13ceddcdaaa3592
raheeclara <138274269+raheeclara@users.noreply.github.com>
2023-07-20 20:32:20 +0900

    Add files via upload

--- python.test.py
+++ python.test.py
@@ -1,15 +1,24 @@
-class Car:
-    def __init__(self,model,color):
-        self.model = model
-        self.color = color
-    def info(self):
-        print("Model :",self.model,",Color:",self.color)
-class CarDrive(Car):
-    def info(self):
-        print("The model of this car is %s."% self.model)
-        print("The color is %s."% self.color)
-    def speedChange(self,v):
-        self.speed = v
-        print("speedChange :",self.speed)    
-bmw = CarDrive("BMW","white")
-bmw.info()
+class TV :
+    def __init__(self,power,channel,volume):
+        self.power = power
+        self.channel = channel
+        self.volume = volume
+    def intro(self):
+        print("Power :",self.power)
+        print("Channel :",self.channel)
+        print("Volume :",self.volume)
+    def trun_off(self):
+        print("Trun off")
+    def trun_on(self):
+        print("Trun on")
+    def set_channel(self,v):
+        self.channel = v
+    def set_volume(self,v):
+        self.volume = v        
+
+lugo = TV("True","0","0")
+lugo.set_channel("1")
+lugo.set_volume("16")
+lugo.intro()
+lugo.trun_off()
+lugo.trun_on()
